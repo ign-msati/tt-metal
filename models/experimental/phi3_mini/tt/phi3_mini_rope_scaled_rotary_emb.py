b06a631e5965664b848e13ec926f49e43c6000a9
Navaneeth Krishnan <navaneeth.krishnan@ignitarium.com>
2025-05-08 07:11:26 +0000

    Updates dtype for rope operation

--- models/experimental/phi3_mini/tt/phi3_mini_rope_scaled_rotary_emb.py
+++ models/experimental/phi3_mini/tt/phi3_mini_rope_scaled_rotary_emb.py
@@ -12,10 +12,10 @@ class TtPhi3MiniLongRoPEScaledRotaryEmbedding(TtPhi3MiniRotaryEmbedding):
     def __init__(self, dim, config, device):
         super().__init__(dim, config.max_position_embeddings, config.rope_theta, device)
 
-        self.short_factor = torch.tensor(config.rope_scaling["short_factor"])
-        self.long_factor = torch.tensor(config.rope_scaling["long_factor"])
+        self.short_factor = torch.tensor(config.rope_scaling["short_factor"], dtype=torch.float32)
+        self.long_factor = torch.tensor(config.rope_scaling["long_factor"], dtype=torch.float32)
         self.original_max_position_embeddings = config.original_max_position_embeddings
-        self.inv_freq_shape = torch.arange(0, self.dim, 2)
+        self.inv_freq_shape = torch.arange(0, self.dim, 2, dtype=torch.int64).float() / self.dim
 
         # Both these are vectors of length 48
         self.inv_freq_long = 1.0 / (self.long_factor * self.base**self.inv_freq_shape)
@@ -32,16 +32,18 @@ class TtPhi3MiniLongRoPEScaledRotaryEmbedding(TtPhi3MiniRotaryEmbedding):
                 self.inv_freq_long[None, :, None].float().expand(position_ids.shape[0], -1, 1),
                 device=self.device,
                 layout=ttnn.TILE_LAYOUT,
+                dtype=ttnn.float32,
             )
-            freqs = ttnn.matmul(self.inv_freq_long_expanded, position_ids)
+            freqs = ttnn.matmul(self.inv_freq_long_expanded, position_ids, dtype=ttnn.float32)
             ttnn.deallocate(self.inv_freq_long_expanded)
         else:
             self.inv_freq_short_expanded = ttnn.from_torch(
                 self.inv_freq_short[None, :, None].float().expand(position_ids.shape[0], -1, 1),
                 device=self.device,
                 layout=ttnn.TILE_LAYOUT,
+                dtype=ttnn.float32,
             )
-            freqs = ttnn.matmul(self.inv_freq_short_expanded, position_ids)
+            freqs = ttnn.matmul(self.inv_freq_short_expanded, position_ids, dtype=ttnn.float32)
             ttnn.deallocate(self.inv_freq_short_expanded)
 
         freqs = ttnn.transpose(freqs, 1, 2)
