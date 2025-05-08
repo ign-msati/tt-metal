2580195b7e4aa9584069a3bccb380130b7328ff4
Navaneeth Krishnan <navaneeth.krishnan@ignitarium.com>
2025-05-08 05:48:47 +0000

    [WIP] Fixes some errors

--- models/experimental/phi3_mini/tt/phi3_mini_rope_scaled_rotary_emb.py
+++ models/experimental/phi3_mini/tt/phi3_mini_rope_scaled_rotary_emb.py
@@ -18,7 +18,7 @@ class TtPhi3MiniLongRoPEScaledRotaryEmbedding(TtPhi3MiniRotaryEmbedding):
         self.inv_freq_shape = torch.arange(0, self.dim, 2)
 
         # Both these are vectors of length 48
-        self.inv_freq_long = 1.0 / (self.long_factor_factor * self.base**self.inv_freq_shape)
+        self.inv_freq_long = 1.0 / (self.long_factor * self.base**self.inv_freq_shape)
         self.inv_freq_short = 1.0 / (self.short_factor * self.base**self.inv_freq_shape)
 
         # These are matrices of shape (131072, 48, 1)
@@ -26,18 +26,18 @@ class TtPhi3MiniLongRoPEScaledRotaryEmbedding(TtPhi3MiniRotaryEmbedding):
         # self.inv_freq_short_expanded = ttnn.from_torch(self.inv_freq_short[None, :, None].float().expand(self.original_max_position_embeddings, -1, 1))
 
     def forward(self, x: ttnn.Tensor, position_ids: ttnn.Tensor, seq_len=None) -> ttnn.Tensor:
-        position_ids_expanded = position_ids[:, None, :]
+        # position_ids_expanded = position_ids[:, None, :] # Our assumption about the shape of position id seems to be wrong
         if seq_len > self.original_max_position_embeddings:
             self.inv_freq_long_expanded = ttnn.from_torch(
                 self.inv_freq_long[None, :, None].float().expand(position_ids.shape[0], -1, 1)
             )
-            freqs = ttnn.matmul(self.inv_freq_expanded_long[: len(position_ids)], position_ids_expanded)
+            freqs = ttnn.matmul(self.inv_freq_long_expanded[: len(position_ids)], position_ids)
             ttnn.deallocate(self.inv_freq_long_expanded)
         else:
             self.inv_freq_short_expanded = ttnn.from_torch(
                 self.inv_freq_short[None, :, None].float().expand(position_ids.shape[0], -1, 1)
             )
-            freqs = ttnn.matmul(self.inv_freq_expanded_short[: len(position_ids)], position_ids_expanded)
+            freqs = ttnn.matmul(self.inv_freq_short_expanded[: len(position_ids)], position_ids)
             ttnn.deallocate(self.inv_freq_short_expanded)
 
         freqs = ttnn.transpose(freqs, 1, 2)
