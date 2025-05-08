abc950e119a9312608e890d0d8a7b192c67be78b
Navaneeth Krishnan <navaneeth.krishnan@ignitarium.com>
2025-05-08 04:51:07 +0000

    [WIP] some optimisation based on conjecture

--- models/experimental/phi3_mini/tt/phi3_mini_rope_scaled_rotary_emb.py
+++ models/experimental/phi3_mini/tt/phi3_mini_rope_scaled_rotary_emb.py
@@ -17,24 +17,30 @@ class TtPhi3MiniLongRoPEScaledRotaryEmbedding(TtPhi3MiniRotaryEmbedding):
         self.original_max_position_embeddings = config.original_max_position_embeddings
         self.inv_freq_shape = torch.arange(0, self.dim, 2)
 
+        # Both these are vectors of length 48
         self.inv_freq_long = 1.0 / (self.long_factor_factor * self.base**self.inv_freq_shape)
         self.inv_freq_short = 1.0 / (self.short_factor * self.base**self.inv_freq_shape)
 
-        self.inv_freq_long_expanded = ttnn.from_torch(
-            self.inv_freq_long[None, :, None].float().expand(self.original_max_position_embeddings, -1, 1)
-        )
-        self.inv_freq_short_expanded = ttnn.from_torch(
-            self.inv_freq_short[None, :, None].float().expand(self.original_max_position_embeddings, -1, 1)
-        )
+        # These are matrices of shape (131072, 48, 1)
+        # self.inv_freq_long_expanded = ttnn.from_torch(self.inv_freq_long[None, :, None].float().expand(self.original_max_position_embeddings, -1, 1))
+        # self.inv_freq_short_expanded = ttnn.from_torch(self.inv_freq_short[None, :, None].float().expand(self.original_max_position_embeddings, -1, 1))
 
     def forward(self, x: ttnn.Tensor, position_ids: ttnn.Tensor, seq_len=None) -> ttnn.Tensor:
         position_ids_expanded = position_ids[:, None, :]
         if seq_len > self.original_max_position_embeddings:
+            self.inv_freq_long_expanded = ttnn.from_torch(
+                self.inv_freq_long[None, :, None].float().expand(position_ids.shape[0], -1, 1)
+            )
             freqs = ttnn.matmul(self.inv_freq_expanded_long[: len(position_ids)], position_ids_expanded)
+            ttnn.deallocate(self.inv_freq_long_expanded)
         else:
+            self.inv_freq_short_expanded = ttnn.from_torch(
+                self.inv_freq_short[None, :, None].float().expand(position_ids.shape[0], -1, 1)
+            )
             freqs = ttnn.matmul(self.inv_freq_expanded_short[: len(position_ids)], position_ids_expanded)
+            ttnn.deallocate(self.inv_freq_short_expanded)
 
-        freqs = ttnn.transpose(1, 2)
+        freqs = ttnn.transpose(freqs, 1, 2)
 
         emb = ttnn.concat([freqs, freqs], dim=-1)
 
