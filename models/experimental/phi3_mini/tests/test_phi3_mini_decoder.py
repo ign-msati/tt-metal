1c7c776e41bc3595b809b3357c0a3dc810d85873
Mayank Sati <ign_msati@untether.ai>
2025-05-09 18:16:19 +0530

    update test for decoder but mismatching

--- models/experimental/phi3_mini/tests/test_phi3_mini_decoder.py
+++ models/experimental/phi3_mini/tests/test_phi3_mini_decoder.py
@@ -3,10 +3,12 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import torch
+import ttnn
 
 from loguru import logger
 
 from models.experimental.phi3_mini.tt.phi3_mini_attention import TtPhi3MiniAttention
+from models.experimental.phi3_mini.tt.phi3_mini_decoder import TtPhi3MiniDecoder
 from models.utility_functions import (
     tt2torch_tensor,
     comp_allclose,
@@ -14,17 +16,29 @@ from models.utility_functions import (
 )
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
-
-def test_phi3_mini_decoder_inference(device):
+def test_phi3_mini_decoder_inference(device=None):
+    if device == None:
+        device = ttnn.open_device(device_id=0)
     torch.manual_seed(1234)
 
     LAYER_INDEX = 0
-    base_address = f"model.layers[{LAYER_INDEX}]"
+    base_address = f"model.layers.{LAYER_INDEX}"
 
-    model = AutoModelForCausalLM.from_pretrained("microsoft/Phi-3-mini-128k-instruct")
+    model = AutoModelForCausalLM.from_pretrained("microsoft/Phi-3-mini-128k-instruct", trust_remote_code=True)
 
     # Torch phi3-mini attn layer
-    torch_model = model.model.layer[LAYER_INDEX]
+    torch_model = model.model.layers[LAYER_INDEX]
+
+
+
+    hidden_size =3072
+    seq_len=3
+    batch=1
+    # hidden_states, tt_hidden_states = create_attention_input(mode, ttnn.bfloat16, batch, seq_len, config.hidden_size, ttnn_device)
+    torch_hidden_states = torch.rand(batch, seq_len, hidden_size)
+    attention_mask = torch.ones(batch, 1, seq_len, seq_len, dtype=torch.bool).tril()
+    torch_position_ids = torch.arange(0, seq_len, 1, dtype=torch.long).unsqueeze(0).repeat(batch, 1)
+    torch_output = torch_model(torch_hidden_states, position_ids=torch_position_ids)
 
     # Tt phi3-mini attn layer
     tt_model = TtPhi3MiniDecoder(
@@ -35,25 +49,25 @@ def test_phi3_mini_decoder_inference(device):
         layer_idx=LAYER_INDEX,
     )
 
-    # Run torch model
-    hidden_states = torch.rand(1, 32, 768)
-    attention_mask = torch.ones(1, 1, 32)
-    torch_output = torch_model(hidden_states, attention_mask=attention_mask)
+    # # Run torch model
+    # hidden_states = torch.rand(1, 32, 768)
+    # attention_mask = torch.ones(1, 1, 32)
+    # torch_output = torch_model(hidden_states, attention_mask=attention_mask)
 
     # Run tt model
     tt_hidden_states = ttnn.from_torch(
-        hidden_states,
+        torch_hidden_states,
         device=device,
         dtype=ttnn.bfloat16,
         layout=ttnn.TILE_LAYOUT,
     )
-    tt_attention_mask = ttnn.from_torch(
-        attention_mask,
-        device=device,
-        dtype=ttnn.bfloat16,
-        layout=ttnn.TILE_LAYOUT,
-    )
-    tt_output = tt_model(tt_hidden_states, attention_mask=tt_attention_mask)
+    # tt_attention_mask = ttnn.from_torch(
+    #     attention_mask,
+    #     device=device,
+    #     dtype=ttnn.bfloat16,
+    #     layout=ttnn.TILE_LAYOUT,
+    # )
+    tt_output = tt_model(tt_hidden_states, position_ids=torch_position_ids)
 
     # Compare outputs
     tt_output_torch = tt2torch_tensor(tt_output[0])
@@ -65,8 +79,12 @@ def test_phi3_mini_decoder_inference(device):
     logger.info(pcc_message)
 
     if does_pass:
-        logger.info("RobertaAttention Passed!")
+        logger.info("phi3decoder Passed!")
     else:
-        logger.warning("RobertaAttention Failed!")
+        logger.warning("phi3decoder Failed!")
 
     assert does_pass
+    # Close device
+    ttnn.close_device(device)
+
+test_phi3_mini_decoder_inference()
--- models/experimental/phi3_mini/tt/phi3_mini_decoder.py
+++ models/experimental/phi3_mini/tt/phi3_mini_decoder.py
@@ -3,16 +3,44 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from typing import Optional, Tuple
-
+import torch
 import ttnn
-
+from models.utility_functions import pad_by_zero
 from models.common.lightweightmodule import LightweightModule
-
-
+from models.experimental.phi3_mini.tt.phi3_mini_attention import TtPhi3MiniAttention
+from models.experimental.phi3_mini.tt.phi3_mini_mlp import TtPhi3MiniMLP
 class TtPhi3MiniDecoder(LightweightModule):
     def __init__(self, config, state_dict, base_address, device, layer_idx):
         super().__init__()
-        pass
+
+            # Tt phi3-mini attn layer
+        SELF_LAYER_INDEX=0
+        self.attention = TtPhi3MiniAttention(
+            config=config,
+            base_address=f"{base_address}.self_attn",
+            # base_address=f"model.layers.{SELF_LAYER_INDEX}.self_attn",
+            device=device,
+            state_dict=state_dict,
+            layer_idx=SELF_LAYER_INDEX,
+        )
+        self.feed_forward = TtPhi3MiniMLP(
+            config=config,
+            base_address=f"{base_address}.mlp",
+            # base_address=f"model.layers.{SELF_LAYER_INDEX}.mlp",
+            device=device,
+            state_dict=state_dict,
+            )
+        # f"model.layers.{SELF_LAYER_INDEX}.input_layernorm.weight"
+        # state_dict['model.layers.0.input_layernorm.weight']
+        # base_address=f"model.layers.{SELF_LAYER_INDEX}"
+        # state_dict['model.layers.0.input_layernorm.weight']
+        # state_dict['model.layers.0.post_attention_layernorm.weight']
+        self.input_layernorm = pad_by_zero(state_dict[f"{base_address}.input_layernorm.weight"], device)[0]
+        self.post_attention_layernorm = pad_by_zero(state_dict[f"{base_address}.post_attention_layernorm.weight"], device)[0]
+        # self.layernorm_weights = f"model.layers.{SELF_LAYER_INDEX}.input_layernorm.weight"
+        # self.layernorm_bias = layernorm_bias
+        self.variance_epsilon=config.rms_norm_eps
+
 
     """
     """
@@ -20,10 +48,31 @@ class TtPhi3MiniDecoder(LightweightModule):
     def forward(
         self,
         hidden_states: torch.Tensor,
-        attention_mask: Optional[torch.Tensor] = None,
+        # attention_mask: Optional[torch.Tensor] = None,
         position_ids: Optional[torch.LongTensor] = None,
-        past_key_value: Optional[Cache] = None,
-        output_attentions: bool = False,
-        use_cache: bool = False,
-    ) -> Tuple[ttnn.Tensor]:
-        pass
+        # past_key_value: Optional[Cache] = None,
+        # output_attentions: bool = False,
+        # use_cache: bool = False,
+        
+        ) -> Tuple[ttnn.Tensor]:
+        # pass
+        residual = hidden_states
+
+        # hidden_states = ttnn.layer_norm(hidden_states, self.layernorm_weights)
+        hidden_states = ttnn.rms_norm(hidden_states, epsilon=self.variance_epsilon, weight=self.input_layernorm)
+
+
+        hidden_states = self.attention(hidden_states, position_ids=position_ids)
+
+        # hidden_states = residual + hidden_states 
+
+        residual = hidden_states
+
+        # hidden_states = ttnn.rms_norm(hidden_states, epsilon=self.variance_epsilon, weight=self.post_attention_layernorm)
+
+        hidden_states = self.feed_forward(hidden_states)
+
+        # # hidden_states = residual + hidden_states
+        return hidden_states
+
+        # ttnn.rms_norm(hidden_states, epsilon=self.variance_epsilon, weight=self.weight)
