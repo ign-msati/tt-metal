3a04816b79c366b2a7a555505c0bbdcc4572918d
Navaneeth Krishnan <navaneeth.krishnan@ignitarium.com>
2025-05-11 15:49:52 +0000

    Fixing long rope test, current pcc 0.99

--- models/experimental/phi3_mini/tests/test_phi3_mini_rope_scaled_rotary_emb.py
+++ models/experimental/phi3_mini/tests/test_phi3_mini_rope_scaled_rotary_emb.py
@@ -18,7 +18,7 @@ def test_phi3_mini_rope_scaled_roatry_emb(batches: int = 1, seq_len: int = 384, 
     if device == None:
         device = ttnn.open_device(device_id=0)
     torch.manual_seed(0)
-    expected_pcc_score = 0.97
+    expected_pcc_score = 0.99
 
     model = AutoModelForCausalLM.from_pretrained("microsoft/Phi-3-mini-128k-instruct", trust_remote_code=True)
 
@@ -26,6 +26,7 @@ def test_phi3_mini_rope_scaled_roatry_emb(batches: int = 1, seq_len: int = 384, 
     torch_model = model.model.layers[0].self_attn.rotary_emb
 
     rotary_dim = model.config.hidden_size // model.config.num_attention_heads
+
     # Tt phi3-mini mlp layer
     tt_model = TtPhi3MiniLongRoPEScaledRotaryEmbedding(
         dim=rotary_dim,
@@ -34,13 +35,13 @@ def test_phi3_mini_rope_scaled_roatry_emb(batches: int = 1, seq_len: int = 384, 
     )
 
     # Run torch model
-    torch_position_ids = torch.arange(0, seq_len, 1, dtype=torch.long).unsqueeze(0)
-    torch_value_states = torch.arange(0, seq_len, 1, dtype=torch.long)
+    torch_position_ids = torch.arange(0, seq_len, 1, dtype=torch.long).unsqueeze(0).repeat(batches, 1).float()
+    torch_value_states = torch.arange(0, seq_len, 1, dtype=torch.long).float()
     torch_output = torch_model(torch_value_states, torch_position_ids)
 
     # Run tt model
     tt_postion_ids = ttnn.from_torch(
-        torch_position_ids[:, None, :].float(),
+        torch_position_ids,
         device=device,
         dtype=ttnn.float32,
         layout=ttnn.TILE_LAYOUT,
